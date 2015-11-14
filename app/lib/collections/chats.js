076d8bee215f18400b5e2836ee3a51766c6a4440
Jenny Lian <jenny@fingerfoodstudios.com>
2015-11-14 14:57:31 -0800

    added the chat collections

---
+++ app/lib/collections/chats.js
@@ -0,0 +1,32 @@
+Chats = new Mongo.Collection('chats');
+
+
+if (Meteor.isServer) {
+  Chats.allow({
+    insert: function (userId, doc) {
+      return false;
+    },
+
+    update: function (userId, doc, fieldNames, modifier) {
+      return false;
+    },
+
+    remove: function (userId, doc) {
+      return false;
+    }
+  });
+
+  Chats.deny({
+    insert: function (userId, doc) {
+      return true;
+    },
+
+    update: function (userId, doc, fieldNames, modifier) {
+      return true;
+    },
+
+    remove: function (userId, doc) {
+      return true;
+    }
+  });
+}
