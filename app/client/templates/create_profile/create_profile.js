ae9fab8c103fdb255fcf5a220b212df6ff617dca
Jenny Lian <jenny@fingerfoodstudios.com>
2015-11-14 19:55:27 -0800

    added the create profile templates for profile creation

---
+++ app/client/templates/create_profile/create_profile.js
@@ -0,0 +1,23 @@
+/*****************************************************************************/
+/* CreateProfile: Event Handlers */
+/*****************************************************************************/
+Template.CreateProfile.events({
+});
+
+/*****************************************************************************/
+/* CreateProfile: Helpers */
+/*****************************************************************************/
+Template.CreateProfile.helpers({
+});
+
+/*****************************************************************************/
+/* CreateProfile: Lifecycle Hooks */
+/*****************************************************************************/
+Template.CreateProfile.onCreated(function () {
+});
+
+Template.CreateProfile.onRendered(function () {
+});
+
+Template.CreateProfile.onDestroyed(function () {
+});
