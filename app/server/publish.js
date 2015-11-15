259cba1e90327f0b0f62d668b7b24a32b086bda6
Jenny Lian <jenny@fingerfoodstudios.com>
2015-11-14 16:24:04 -0800

    just to make sure it runs

--- app/server/publish.js
+++ app/server/publish.js
@@ -1,1 +1,8 @@
-
+Meteor.publish("userData", function () {
+  if (this.userId) {
+    return Meteor.users.find({_id: this.userId},
+                             {fields: {'public': 1}});
+  } else {
+    this.ready();
+  }
+});
