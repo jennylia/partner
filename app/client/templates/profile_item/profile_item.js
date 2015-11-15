ef435cef62c9510cba746cf70b141bf8f4b90011
Jenny Lian <jenny@fingerfoodstudios.com>
2015-11-14 16:43:58 -0800

    can add likes now

--- app/client/templates/profile_item/profile_item.js
+++ app/client/templates/profile_item/profile_item.js
@@ -8,7 +8,27 @@ Template.ProfileItem.events({
 	},
 
 	'click [name="like"]': function(e, tmpl){
-		console.log("like");
+
+		// need to find the profile that belongs to this user
+		var pid = Profiles.find({owner_id:Meteor.userId()}).fetch()[0]._id;
+		console.log(pid);
+		//To hack the problem with secure packages, we are going to store this in the profiles
+		var Obj = Profiles.find({owner_id:Meteor.userId()}).fetch();
+		// console.log(Obj);
+		var likes = Profiles.find({owner_id:Meteor.userId()}).fetch()[0].likes;
+		var updatedLikes;
+		if (likes){
+			updatedLikes = likes;
+			updatedLikes.push(this._id);
+
+		}else{
+			updatedLikes = [];
+			updatedLikes.push(this._id);
+
+		}
+		console.log(updatedLikes);
+
+		Profiles.update({_id: pid}, {$set: {likes: updatedLikes}});
 	}
 });
 
