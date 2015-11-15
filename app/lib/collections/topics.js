3c32b9489239f36872ff12bec5ab0d5703bf6437
Jenny Lian <jenny@fingerfoodstudios.com>
2015-11-14 17:03:10 -0800

    added all the topics

---
+++ app/lib/collections/topics.js
@@ -0,0 +1,32 @@
+Topics = new Mongo.Collection('topics');
+
+
+if (Meteor.isServer) {
+  Topics.allow({
+    insert: function (userId, doc) {
+      return true;
+    },
+
+    update: function (userId, doc, fieldNames, modifier) {
+      return true;
+    },
+
+    remove: function (userId, doc) {
+      return true;
+    }
+  });
+
+  // Topics.deny({
+  //   insert: function (userId, doc) {
+  //     return true;
+  //   },
+
+  //   update: function (userId, doc, fieldNames, modifier) {
+  //     return true;
+  //   },
+
+  //   remove: function (userId, doc) {
+  //     return true;
+  //   }
+  // });
+}
