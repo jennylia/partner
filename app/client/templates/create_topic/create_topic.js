5501ef257f3ada6055319541d197a7d709f0e7bc
Jenny Lian <jenny@fingerfoodstudios.com>
2015-11-14 17:42:12 -0800

    okay somewhat done I guess with the topics

---
+++ app/client/templates/create_topic/create_topic.js
@@ -0,0 +1,40 @@
+/*****************************************************************************/
+/* CreateTopic: Event Handlers */
+/*****************************************************************************/
+Template.CreateTopic.events({
+	'submit form': function(e, tmpl){
+		e.preventDefault();
+
+		var subject = tmpl.find('#topic').value;
+		var description = tmpl.find('#description').value;
+		console.log(subject);
+		console.log(description);
+
+		Topics.insert({
+			name: subject,
+			description: description,
+			host: Meteor.userId,
+			hostname: Meteor.user().emails[0].address
+		})
+
+Topics.insert({ name: "CPSC 121 Assignment", description: "whatever is important I guess", host: Meteor.userId() })
+	}
+});
+
+/*****************************************************************************/
+/* CreateTopic: Helpers */
+/*****************************************************************************/
+Template.CreateTopic.helpers({
+});
+
+/*****************************************************************************/
+/* CreateTopic: Lifecycle Hooks */
+/*****************************************************************************/
+Template.CreateTopic.onCreated(function () {
+});
+
+Template.CreateTopic.onRendered(function () {
+});
+
+Template.CreateTopic.onDestroyed(function () {
+});
