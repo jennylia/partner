9448b988d7b1c5312d5342e8e7144b677572608a
Jenny Lian <jenny@fingerfoodstudios.com>
2015-11-18 22:36:37 -0800

    finally done the matching part with help of lodash

--- app/client/templates/profile_item/profile_item.js
+++ app/client/templates/profile_item/profile_item.js
@@ -20,27 +20,65 @@ Template.ProfileItem.events({
 		var updatedLikes;
 		if (likes){
 			updatedLikes = likes;
-			updatedLikes.push(this._id);
-
+			var hasit = lodash.includes(likes, this._id);
+			if (!hasit){
+				updatedLikes.push(this._id);
+			}
 		}else{
 			updatedLikes = [];
 			updatedLikes.push(this._id);
-
 		}
-		console.log(updatedLikes);
+		// console.log(updatedLikes);
 
 		Profiles.update({_id: pid}, {$set: {likes: updatedLikes}});
 
 
-
 		// Over here we will also calculate to see if we can create a match
-		findMatch();
-		var a = [1,2,3];
-		console.log(a);
+		findInsertMatch(pid, this._id);
 
-		function findMatch(){
-			console.log("meh");
-		}
+		function findInsertMatch(myId, theirId){
+			var thierLikes = Profiles.findOne({_id:theirId}).likes;
+			var likeYouBack = lodash.includes(thierLikes, myId);
+
+			// The below code will only get excuted if they like you back!
+			if (likeYouBack){
+				//we need to do this smarter
+				var existMatch1 = Matches.findOne({from_id: pid, to_id: theirId});
+				var existMatch2 = Matches.findOne({from_id: theirId, to_id: pid});
+				var existMatch = existMatch1 || existMatch2;
+
+				if (existMatch){
+					console.log(existMatch);
+					console.log("this match already exist");
+				}else{
+					console.log("this match gotta be created");
+					Matches.insert({from_id: myId, to_id: theirId});
+					Matches.insert({from_id: theirId, to_id: myId});
+				}
+			}
+		};
+
+		// function findInsertMatch(likes, pid){
+		// 	likes.forEach(function(like) {
+		// 		console.log(like);
+		// 		//who do they like
+		// 		var thierLikes = Profiles.findOne({_id:like}).likes;
+		// 		//do they like you back
+		// 		var likeYouBack = lodash.includes(thierLikes, pid);
+
+		// 		//if they like you back, we insert a match
+		// 		if (likeYouBack){
+		// 			//does this match exit?
+		// 			var existMatch = Matches.find({from_id: pid, to_id: like});
+		// 			if (existMatch){
+		// 				console.log("Match already exist")
+		// 			}else{
+		// 				Matches.insert({from_id: pid, to_id: like});
+
+		// 			}
+		// 		}
+		// 	});			
+		// };
 	}
 });
 
