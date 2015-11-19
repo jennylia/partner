a8114918b1f7f779b444dd01cbbc8314bbcb18a0
Jenny Lian <jenny@fingerfoodstudios.com>
2015-11-18 21:25:53 -0800

    created the match list and match items, now need to think about how to insert a match

--- app/client/templates/match_list/match_list.js
+++ app/client/templates/match_list/match_list.js
@@ -8,9 +8,26 @@ Template.MatchList.events({
 /* MatchList: Helpers */
 /*****************************************************************************/
 Template.MatchList.helpers({
+	// This is a customized view...
+	// You are looking up all the profiles that are matched by this user 
+	//and that are being matches back
 	matches: function(){
-		return Matches.find();
+		var curProfile = Profiles.find({'owner_id': Meteor.userId()}).fetch()[0];
+		var profileId = curProfile._id;
+		var myMatches = Matches.find({from_id: profileId}).fetch();
+
+		var myMatchedProfiles = [];
+		myMatches.forEach(function(data){
+			var lookupId = data.to_id;
+
+			var tempProfile = Profiles.findOne({'_id': lookupId});
+			if (tempProfile){
+				myMatchedProfiles.push(tempProfile);
+			}
+		});
+		return myMatchedProfiles;
 	}
+
 });
 
 /*****************************************************************************/
